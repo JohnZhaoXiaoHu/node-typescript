dc868aac4e8680e5f3eca074a52c58846559ba9f
sarmat <sarmatkasaev@gmail.com>
2019-04-22 13:49:55 +0300

    PM2

---
+++ src/Server.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import { Client } from 'pg';
+import { PORT } from './config';
+import Postgres from './db/Postgres';
+import { ItemsRoutes } from './routes/ItemsRoutes';
+
+class Server {
+  public app: express.Application;
+  private itemsRouter: express.Router;
+  private pgClient: Client;
+
+  constructor() {
+    this.app = express();
+    this.itemsRouter = new ItemsRoutes().router;
+    this.pgClient = new Postgres().client;
+
+    this.configRoutes();
+
+    this.pgClient.connect((err) => {
+      if (err) {
+          return console.error('failed connection to Postgres', err.stack);
+      }
+      console.log('successful connected to Postgres');
+
+      this.app.listen( PORT, () => {
+        console.log(`Server started at http://localhost:${ PORT }`);
+      });
+    });
+  }
+
+  private configRoutes(): void {
+    this.app.get( '/', ( req, res ) => {
+      res.send( 'Hello typescript!' );
+    });
+
+    // items
+    this.app.use( '/items', this.itemsRouter);
+  }
+}
+export default new Server();
