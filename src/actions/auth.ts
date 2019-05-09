d04895475e10653a018013a006f8bde7c7cfad3c
sarmat <sarmatkasaev@gmail.com>
2019-05-09 14:03:17 +0300

    async/await edits

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -5,37 +5,41 @@ import { TOKEN_KEY } from '../config';
 import User from '../db/models/User';
 
 export default {
-  giveAToken: (ctx: RouterContext, next: () => Promise<any>) => {
+  giveAToken: async (ctx: RouterContext, next: () => Promise<any>) => {
     const user = {
       mail: ctx.request.body.mail,
       pwd: ctx.request.body.password
     };
-    try {
-      const token = jwt.sign({ user }, TOKEN_KEY);
-      const password = user.pwd;
+    const token = jwt.sign({ user }, TOKEN_KEY);
+    const password = user.pwd;
 
-      return User.findOne({
+    let dbUser: User;
+    try {
+      dbUser = await User.findOne({
         where: {
           mail: user.mail,
         }
-      }).then((usr: User) => {
-        const hashedPwd = usr.get('password') as string;
-        return bcrypt.compare(password, hashedPwd).then((res) => {
-          if (res) {
-            ctx.body = token;
-          } else {
-            ctx.status = 401;
-            ctx.body = 'wrong password';
-          }
-        });
-      }).catch((err) => {
-        ctx.status = 500;
-        ctx.body = `error getting user`;
-        console.log(err);
       });
-    } catch {
-      ctx.status = 401;
-      ctx.body = 'error getting token';
+    } catch (err) {
+      ctx.status = 404;
+      ctx.body = `user not found`;
+      return;
     }
+    const hashedPwd = dbUser.get('password') as string;
+
+    let match: boolean;
+    try {
+      match = await bcrypt.compare(password, hashedPwd);
+    } catch (err) {
+      ctx.status = 500;
+      console.log(err);
+      return;
+    }
+    if (!match) {
+      ctx.status = 401;
+      ctx.body = 'wrong password';
+      return;
+    }
+    ctx.body = token;
   }
 };
--- src/actions/user.ts
+++ src/actions/user.ts
@@ -6,53 +6,53 @@ import { BCRYPT_SALT, TOKEN_KEY } from '../config';
 import User from '../db/models/User';
 
 export default {
-  createUser: (ctx: RouterContext, next: () => Promise<any>) => {
+  createUser: async (ctx: RouterContext, next: () => Promise<any>) => {
     const user = {
       mail: ctx.request.body.mail,
       pwd: ctx.request.body.password
     };
-    try {
-      const token = jwt.sign({ user }, TOKEN_KEY);
-      const saltRounds = BCRYPT_SALT as number;
-      const password = user.pwd;
-      const salt = bcrypt.genSaltSync(saltRounds);
-      const hash = bcrypt.hashSync(password, salt);
+    const token = jwt.sign({ user }, TOKEN_KEY);
+    const saltRounds = BCRYPT_SALT as number;
+    const password = user.pwd;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(password, salt);
 
-      return User.create({
+    let createdUser: User;
+    try {
+      createdUser = await User.create({
         id: uuid(),
         mail: user.mail,
-        password: hash,
-      }).then((inserted) => {
-        const newUser = inserted.toJSON();
-        ctx.status = 201;
-        ctx.body = {
-          token,
-          user: {...newUser}
-        };
-      }).catch((err) => {
-        ctx.status = 400;
-        ctx.body = `error adding user in db`;
-        console.log(err);
+        password: hash
       });
-    } catch {
-      ctx.status = 401;
-      ctx.body = 'error getting token';
+    } catch (err) {
+      ctx.status = 400;
+      ctx.body = `error adding user in db`;
+      return;
     }
+    const newUser = createdUser.toJSON();
+    ctx.status = 201;
+    ctx.body = {
+      token,
+      user: {...newUser}
+    };
   },
 
-  getUserData: (ctx: RouterContext, next: () => Promise<any>) => {
+  getUserData: async (ctx: RouterContext, next: () => Promise<any>) => {
     const token = ctx.token;
+
+    const decoded = jwt.verify(token, TOKEN_KEY) as any;
+    const mail = decoded.user.mail;
+
+    let dbUser: User;
     try {
-      const decoded = jwt.verify(token, TOKEN_KEY) as any;
-      const mail = decoded.user.mail;
-      return User.findOne({
+      dbUser = await User.findOne({
         where: { mail }
-      }).then((user: User) => {
-        ctx.body = user;
       });
-    } catch {
-      ctx.status = 401;
-      ctx.body = 'token error';
+    } catch (err) {
+      ctx.status = 404;
+      ctx.body = `user not found`;
+      return;
     }
+    ctx.body = dbUser;
   }
 };
